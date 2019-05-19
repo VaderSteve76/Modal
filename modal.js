92e1984a2f0221fa8fefd9f64938f460569962ef
Steve Alverson <vadersteve76@Steves-Mac-mini.cfl.rr.com>
2019-05-19 14:10:26 -0400

    Close button working

--- modal.js
+++ modal.js
@@ -3,12 +3,20 @@ const modal = document.getElementById('simpleModal');
 //get open modal button
 const modalBtn = document.getElementById('modalBtn');
 // get close button
-const closeBtn = document.getElementById('closeBtn');
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-//listen for click
+//listen for open click
 modalBtn.addEventListener('click', openModal);
+
+//listen for close click
+closeBtn.addEventListener('click', closeModal);
 
 //function to open modal
 function openModal() {
   modal.style.display = 'block';
+}
+
+//function to close modal
+function closeModal() {
+  modal.style.display = 'none';
 }
