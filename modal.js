66621d7e3591ffd259bc9c0b1235c01e0af4af9e
Steve Alverson <vadersteve76@Steves-Mac-mini.cfl.rr.com>
2019-05-19 22:09:31 -0400

    Index2.html & Index2.css

--- modal.js
+++ modal.js
@@ -11,6 +11,9 @@ modalBtn.addEventListener('click', openModal);
 //listen for close click
 closeBtn.addEventListener('click', closeModal);
 
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
 //function to open modal
 function openModal() {
   modal.style.display = 'block';
@@ -19,4 +22,11 @@ function openModal() {
 //function to close modal
 function closeModal() {
   modal.style.display = 'none';
+}
+
+//function to close modal if outside click
+function outsideClick(e) {
+  if(e.target == modal) {
+  modal.style.display = 'none';
+  }
 }
